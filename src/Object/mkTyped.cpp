5f8dffeb8468f29e1b67bcd96c6e8592a3181150
NovemberMonk <hugo.amiard@laposte.net>
2015-03-02 16:04:59 +0100

    Fixed a missing export

--- src/Object/mkTyped.cpp
+++ src/Object/mkTyped.cpp
@@ -24,4 +24,5 @@ namespace mk
 	template class MK_OBJECT_EXPORT Typed<std::vector<Object*>>;
 
 	template class MK_OBJECT_EXPORT Typed<Array<Object>>;
+	template class MK_OBJECT_EXPORT StoreObserver<Object>;
 }
