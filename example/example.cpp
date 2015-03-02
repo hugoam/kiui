a42228ba4197b49e64412c2677a6d0ad6cee6d0a
NovemberMonk <hugo.amiard@laposte.net>
2015-03-03 00:56:54 +0100

    Updated CMake system with example

---
+++ example/example.cpp
@@ -0,0 +1,24 @@
+//  Copyright (c) 2015 Hugo Amiard hugo.amiard@laposte.net
+//  This software is provided 'as-is' under the zlib License, see the LICENSE.txt file.
+//  This notice and the license may not be removed or altered from any source distribution.
+
+#include <Ui/mkUiConfig.h>
+#include <Ui/mkUiTypes.h>
+
+#include <Ui/mkUi.h>
+
+#include <Ui/Nano/mkGlWindow.h>
+
+int main(int argc, char *argv[])
+{
+	std::unique_ptr<mk::GlWindow> glwindow = std::make_unique<mk::GlWindow>(1200, 800, "mk UiEditApp", "../Data/interface");
+	glwindow->initContext();
+
+	mk::UiWindow* uiwindow = glwindow->uiWindow();
+	mk::Form* root = uiwindow->rootForm();
+	createTestWindow(root);
+
+	bool pursue = true;
+	while(pursue)
+		pursue = glwindow->renderFrame();
+}
